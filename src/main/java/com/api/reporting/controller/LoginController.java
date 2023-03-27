180905091194801616ac0f20b4b5cea4187e6b6e
JoonYoung <moter2588@gmail.com>
2023-03-27 17:43:57 +0900

    회원 가입 관련 컨트롤러 1. ("/signup") get - 회원가입 페이지로 이동
    2. ("/join") post
    - 회원가입 정보 insert
    
    3. ("/userIdCheck") post
    - 유저 아이디 중복값 체크
    
    4. ("/userEmailCheck") get
    - 유저 이메일 인증코드 전송

--- src/main/java/com/api/reporting/controller/LoginController.java
+++ src/main/java/com/api/reporting/controller/LoginController.java
@@ -1,28 +1,39 @@
 package com.api.reporting.controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.api.reporting.dto.LoginDTO;
+import com.api.reporting.dto.MemberVO;
 import com.api.reporting.dto.PageMaker;
 import com.api.reporting.dto.SearchCriteria;
 import com.api.reporting.dto.UserVO;
 import com.api.reporting.interceptor.Auth;
 import com.api.reporting.interceptor.Auth.Role;
 import com.api.reporting.service.AdminService;
+import com.api.reporting.service.UserJoinService;
 import com.api.reporting.service.UserService;
 import com.api.reporting.service.UtilService;
 
@@ -41,6 +52,9 @@ public class LoginController {
 	@Inject
 	private UtilService utilService;
 	
+	@Inject
+	private UserJoinService joinService;
+	
 	@Auth(role=Role.ADMIN)
 	@RequestMapping(value = "/show", method = RequestMethod.GET)
 	public String user(Locale locale, Model model) throws Exception{
@@ -57,17 +71,14 @@ public class LoginController {
 	
 	@RequestMapping(value="/login")
 	public void loginGet(@ModelAttribute("dto") LoginDTO dto){
-		logger.info("Call LoginPage.........");
+		logger.info("Call LoginPage.........!!");
 	}
 	
 	@RequestMapping(value = "/a", method = RequestMethod.GET)
 	public String a(LoginDTO dto,HttpSession session, Model model) throws Exception {
 
 		return "/user/a";
-		
-		
 	}
-	
 	
 	@RequestMapping(value = "/loginCheck", method = RequestMethod.POST)
 	public void loginCheck(LoginDTO dto,HttpSession session, Model model) throws Exception {
@@ -94,23 +105,71 @@ public class LoginController {
 		String site_id = vo.getSite_id(); 
 		
 		model.addAttribute("site_id",site_id);
-		
-		
 	}
 
 	
 	@RequestMapping(value="/logout", method = RequestMethod.GET)
     public String logout(HttpSession session, Model model)
     {
-        //�α׾ƿ� ���ǿ� �ִ� ���� ����� �Ͱ� ����.
-        //delete�ϸ� redirect
         logger.info("�α׾ƿ�  ����");
         session.removeAttribute("LOGIN");
 		session.removeAttribute("loginyn");
         logger.info("�α׾ƿ�  ����");
         return "redirect:/user/login";
     }
-
+	
+	@RequestMapping(value="/signup", method = RequestMethod.GET)
+	public void signUp(HttpSession session) {
+		
+		logger.info("signup page");
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/join", method = RequestMethod.POST)
+	public Map<String, String> join(@RequestParam Map<String, Object> map) {
+		
+		logger.info("유저 명 : " + map.get("user_nm").toString());
+		logger.info("유저 아이디 : " + map.get("user_id").toString());
+		map.put("authrt_id", "test");
+		map.put("site_id", "test");
+		
+		MemberVO vo = new MemberVO();
+		
+		vo.setUser_nm(map.get("user_nm").toString());
+		vo.setUser_id(map.get("user_id").toString());
+		vo.setPswd(map.get("pswd").toString());
+		vo.setUser_eml_addr(map.get("user_eml_addr").toString());
+		vo.setAuthrt_id("test");
+		vo.setSite_id("test");
+		
+		Map<String, String> res = new HashMap<String, String>();
+		String result = joinService.join(vo);
+		
+		res.put("result", result);
+		// 다시 로그인 화면
+		return res;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/userIdCheck", method = RequestMethod.POST)
+	public Map<String, Object> userIdCheck(@RequestParam Map<String, String> params, HttpServletRequest req, HttpServletResponse res) throws Exception {
+		logger.info(params.toString());
+		
+		int count = joinService.userIdCheck(params.get("user_id"));
+		Map<String, Object> result = new HashMap<String, Object>();
+		result.put("count", count);
+		
+		return result;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/userEmailCheck", method = RequestMethod.GET)
+	public String userEmailCheck(String email) throws Exception {
+		logger.info("sending email complete!!");
+		logger.info("your email is " + email);
+		
+		return joinService.makeCheckEmail(email);
+	}
 
 	@RequestMapping(value="/read-information", method = RequestMethod.GET)
 	public String readUser(HttpSession session, @ModelAttribute("cri") SearchCriteria cri, Model model) {
