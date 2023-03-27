3c932d6bcca324ba7c3ed6b435d1753bcb067e6c
JoonYoung <moter2588@gmail.com>
2023-03-27 17:45:38 +0900

    유저 DAO 추가
    1. 아이디 중복 확인
    
    2. 회원가입

--- src/main/java/com/api/reporting/dao/UserDAO.java
+++ src/main/java/com/api/reporting/dao/UserDAO.java
@@ -3,6 +3,7 @@ package com.api.reporting.dao;
 import java.util.List;
 
 import com.api.reporting.dto.LoginDTO;
+import com.api.reporting.dto.MemberVO;
 import com.api.reporting.dto.UserVO;
 
 public interface UserDAO {
@@ -10,5 +11,8 @@ public interface UserDAO {
 	public List<UserVO> select_user() throws Exception;
 	
 	public UserVO loginDAO(LoginDTO dto) throws Exception;
+
+	public int userIdCheck(String user_id);
 	
+	public int join(MemberVO vo);
 }
--- src/main/java/com/api/reporting/dao/UserDAOImpl.java
+++ src/main/java/com/api/reporting/dao/UserDAOImpl.java
@@ -1,18 +1,25 @@
 package com.api.reporting.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Repository;
 
+import com.api.reporting.controller.LoginController;
 import com.api.reporting.dto.LoginDTO;
+import com.api.reporting.dto.MemberVO;
 import com.api.reporting.dto.UserVO;
 
 @Repository
 public class UserDAOImpl implements UserDAO {
-
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+	
 	@Inject
 	private SqlSession sqlSession;
 	
@@ -29,5 +36,29 @@ public class UserDAOImpl implements UserDAO {
 		// TODO Auto-generated method stub
 		return sqlSession.selectOne(namespace+".login", dto);
 	}
+	
+	@Override
+	public int userIdCheck(String user_id) {
+		logger.info("@Repository : " + user_id);
+		int count = 0;
+		
+		try {
+			count = sqlSession.selectOne(namespace+".userIdCheck", user_id);
+			logger.info("mapper connection Success!!");
+			
+		} catch (DataAccessException e) {
+			logger.info("@result : " + count);
+			logger.error(e.toString());
+		}
+		
+		return count;
+	}
 
+	@Override
+	public int join(MemberVO vo) {
+		// TODO Auto-generated method stub
+		int count = sqlSession.insert(namespace+".join", vo);
+		logger.info("회원 가입 성공 여부 : " + count + "====");
+		return count;
+	}
 }
